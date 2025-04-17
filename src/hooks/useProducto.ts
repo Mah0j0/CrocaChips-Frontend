40d7fb4d73546281c3e137009c61b1a27165bab3
Maho Salinas <spm2028248@est.univalle.edu>
2025-04-17 13:14:30 -0400

    uso de componente en productos

---
+++ src/hooks/useProducto.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProduct, getProducts } from "../api/ProductosApi.ts";
+import { Producto } from "../types/productos.ts";
+
+export function useProduct() {
+    return useQuery<Producto>({
+        queryKey: ["producto"],
+        queryFn: getProduct,
+        retry: 1,
+        //para no volver a cargar los datos si no han pasado 5 minutos
+        refetchOnWindowFocus: false,
+        staleTime: 5 * 60 * 1000,
+    });
+}
+
+export function useProducts() {
+    return useQuery<Producto[]>({
+        queryKey: ["productos"],
+        queryFn: getProducts,
+        //sirve para no volver a cargar los datos si no han pasado 5 minutos
+        staleTime: 1000 * 60 * 5,
+    });
+}
