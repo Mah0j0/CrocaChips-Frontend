cc08f5e7ef12a082000269ef2b8aa8c45b406f87
Alejandro <cra2028252@est.univalle.edu>
2025-04-21 22:45:52 -0400

    Primera Vercion Funcional Lsto para Presentacion

--- src/pages/UserProfiles.tsx
+++ src/pages/UserProfiles.tsx
@@ -12,7 +12,7 @@ export default function UserProfiles() {
                 title="React.js Profile Dashboard | TailAdmin - Next.js Admin Dashboard Template"
                 description="This is React.js Profile Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
             />
-            <PageBreadcrumb pageTitle="Profile" />
+            <PageBreadcrumb pageTitle="Mi Perfil" />
             <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
 
                 <div className="space-y-6">
