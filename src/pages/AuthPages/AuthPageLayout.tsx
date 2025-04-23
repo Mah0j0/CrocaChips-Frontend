9250a5dae101e99948601ea427634285797607ff
Alejandro <cra2028252@est.univalle.edu>
2025-04-22 22:03:59 -0400

    Actulizacion de logos

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -12,7 +12,14 @@ export default function AuthLayout({
     <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
       <div className="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
         {children}
-        <div className="items-center hidden w-full h-full lg:w-1/2 bg-brand-950 dark:bg-white/5 lg:grid">
+        <div className="items-center hidden w-full h-full lg:w-1/2 bg-[#f15a24] dark:bg-white/5 lg:grid"
+             style={{
+               backgroundImage: "url('/images/logo/inicio-sesion.svg')",
+               backgroundSize: "cover",
+               backgroundRepeat: "no-repeat",
+               backgroundPosition: "center",
+             }}
+        >
           <div className="relative flex items-center justify-center z-1">
             {/* <!-- ===== Common Grid Shape Start ===== --> */}
             <GridShape />
@@ -25,9 +32,6 @@ export default function AuthLayout({
                   alt="Logo"
                 />
               </Link>
-              <p className="text-center text-gray-400 dark:text-white/60">
-                Free and Open-Source Tailwind CSS Admin Dashboard Template
-              </p>
             </div>
           </div>
         </div>
