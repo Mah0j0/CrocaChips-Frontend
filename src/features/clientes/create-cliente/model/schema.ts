cfc5f70512da6a6481a5126a3b5485d109d05f5f
Alejandro <cra2028252@est.univalle.edu>
2025-05-14 10:28:04 -0400

    validacion de nombre cliente

--- src/features/clientes/create-cliente/model/schema.ts
+++ src/features/clientes/create-cliente/model/schema.ts
@@ -2,10 +2,13 @@ import { z } from "zod";
 
 export const clienteCreateSchema = z.object({
     //id_cliente: z.number(),
-    nombre: z.string().min(1, "El nombre es obligatorio"),
+    nombre: z.string()
+        .min(1, "El nombre es obligatorio")
+        .max(50, "El nombre no debe exceder los 50 caracteres")
+        .regex(/^[a-zA-Z\s]+$/, "El nombre solo debe contener letras y espacios")
+        .trim(),
     direccion: z.string().min(1, "La dirección es obligatoria"),
     telefono: z.string().regex(/^[0-9]{8}$/, "El teléfono debe tener 8 dígitos"),
-    //habilitado: z.boolean(),
     carnet: z.string().regex(/^[0-9]{8}$/, "El carnet debe tener 8 dígitos"),
     //created_at: z.date(),
     //imagen: z.string().url("La imagen debe ser una URL válida"),
