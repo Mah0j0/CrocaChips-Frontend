b1fe53f78ae623687cd66cfd9baa52f730bae991
Maho Salinas <spm2028248@est.univalle.edu>
2025-05-14 21:12:13 -0400

    Add create, edit, and delete reception features with corresponding hooks and UI components

---
+++ src/features/recepciones/index.ts
@@ -0,0 +1,3 @@
+//export {default as CreateVentaModal} from "./create-recepcion";
+// export { default as VentaTale } from './table/VentaTable';
+// export { default as VentaFilters } from './filters/VentaFilters';
